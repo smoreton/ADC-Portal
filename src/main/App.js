bb88c31c5dc1e789a8225f46250cdb7d6454a63b
Shefik <caner.shefik@capgemini.com>
2017-05-18 16:49:15 +0100

    Routing to multiple pages

--- src/main/App.js
+++ src/main/App.js
@@ -1,21 +1,33 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserHistory} from 'react-router';
+import HomeTest from './components/HomeTest';
+import CatalogueTest from './components/CatalogueTest';
+import ContactTest from './components/ContactTest';
+import CartTest from './components/CartTest';
+
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
+    render() {
+        let browserHistory = BrowserHistory;
+        return (
+
+            <Router history={browserHistory}>
+                <div>
+                    <Route path="/" component={HomeTest}/>
+                    <Route path="/catalogue" component={CatalogueTest}/>
+                    <Route path="/contact" component={ContactTest}/>
+                    <Route path="/cart" component={CartTest}/>
+                </div>
+
+            </Router>
+
+        )
+            ;
+    }
+}
 export default App;
---
+++ src/main/components/ContactTest.js
@@ -0,0 +1,16 @@
+/**
+ * Created by CSHEFIK on 18/05/2017.
+ */
+import React, { Component } from "react";
+
+
+class ContactTest extends Component {
+    render() {
+        return (
+            <div>Contact Page</div>
+
+        );
+    }
+}
+
+export default ContactTest;
