9b9dde1a18520e7b79ea27591ef26c30ed0c6d78
Eade <sam.eade@capgemini.com>
2017-05-18 17:25:50 +0100

    included initial nav bar impl

--- src/main/App.js
+++ src/main/App.js
@@ -1,33 +1,34 @@
-
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, browserHistory } from 'react-router-dom';
 import {BrowserHistory} from 'react-router';
-import HomeTest from './components/HomeTest';
-import CatalogueTest from './components/CatalogueTest';
-import ContactTest from './components/ContactTest';
-import CartTest from './components/CartTest';
+import styled from "styled-components";
+import Home from './components/Home';
+import Catalogue from './components/Catalogue';
+import Contact from './components/Contact';
+import Cart from './components/Cart';
+import NavLinks, { NavLinkItems } from './components/NavLinks';
 
-
-
+const NavBar = styled.div`
+//insert styling here
+`;
 
 class App extends Component {
 
     render() {
-        let browserHistory = BrowserHistory;
         return (
-
             <Router history={browserHistory}>
+                <NavBar>
+                    <NavLinks/>
+                </NavBar>
                 <div>
-                    <Route path="/" component={HomeTest}/>
-                    <Route path="/catalogue" component={CatalogueTest}/>
-                    <Route path="/contact" component={ContactTest}/>
-                    <Route path="/cart" component={CartTest}/>
+                    <Route path="/" component={Home}/>
+                    <Route path="/catalogue" component={Catalogue}/>
+                    <Route path="/contact" component={Contact}/>
+                    <Route path="/cart" component={Cart}/>
                 </div>
-
             </Router>
-
-        )
-            ;
+        );
     }
 }
+
 export default App;
---
+++ src/main/components/NavLinks.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const InlineList = styled.ul`
+//insert styling here
+`;
+
+const NavLinkText = styled.li`
+//insert styling here
+`;
+
+export const NavLinkItems = [
+    {
+        link: "/",
+        name: "Home"
+    },
+    {
+        link: "/catalogue",
+        name: "Service Catalogue"
+    },
+    {
+        link: "/contact",
+        name: "Contact Us"
+    },
+    {
+        link: "/cart",
+        name: "Service Cart"
+    }
+];
+
+class NavLinks extends Component {
+    render() {
+        return (
+            <InlineList>
+                {NavLinkItems.map(item => (
+                    <NavLinkText key={item.name}>
+                        <Link to={item.link}>
+                            {item.name}
+                        </Link>
+                    </NavLinkText>
+                ))}
+            </InlineList>
+        );
+    }
+}
+
+export default NavLinks;
