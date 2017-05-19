65fd266b67ab792e2c7e85325448111ab8fb6195
Joe Dowd <dowdja@aston.ac.uk>
2017-05-19 16:46:13 +0100

    lint

--- src/main/components/ContactTest.js
+++ src/main/components/ContactTest.js
@@ -1,15 +1,15 @@
 /**
  * Created by CSHEFIK on 18/05/2017.
  */
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 
 class ContactTest extends Component {
-    render() {
-        return (
-            <div>Contact Page</div>
-        );
-    }
+  render() {
+    return (
+      <div>Contact Page</div>
+    );
+  }
 }
 
 export default ContactTest;
--- src/main/components/HomeTest.js
+++ src/main/components/HomeTest.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Column, Row } from 'react-foundation';
 
 import '../App.css';
 
 class HomeTest extends Component {
-    render() {
-        return (
-            <Row className="display">
-                <Column small={12}>Home Page</Column>
-            </Row>
-        );
-    }
+  render() {
+    return (
+      <Row className="display">
+        <Column small={12}>Home Page</Column>
+      </Row>
+    );
+  }
 }
 
 export default HomeTest;
