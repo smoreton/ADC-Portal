5f46133f1d1d00655c5e57e929b572f117da6518
Joe Dowd <dowdja@aston.ac.uk>
2017-05-19 17:14:31 +0100

    lint

--- src/main/model/contact.js
+++ src/main/model/contact.js
@@ -3,36 +3,36 @@
  */
 
 export default class Contact {
-    _firstName;
-    _surname;
-    _email;
-    _telephone;
+  _firstName;
+  _surname;
+  _email;
+  _telephone;
 
-    constructor(
-        firstName,
-        surname,
-        email,
-        telephone
-    ){
-        this._firstName = firstName;
-        this._surname = surname;
-        this._email = email;
-        this._telephone = telephone;
-    }
+  constructor (
+    firstName,
+    surname,
+    email,
+    telephone
+  ) {
+    this._firstName = firstName;
+    this._surname = surname;
+    this._email = email;
+    this._telephone = telephone;
+  }
 
-    get firstName() {
-        return this._firstName;
-    }
+  get firstName() {
+    return this._firstName;
+  }
 
-    get surname() {
-        return this._surname;
-    }
+  get surname() {
+    return this._surname;
+  }
 
-    get email() {
-        return this._email;
-    }
+  get email() {
+    return this._email;
+  }
 
-    get telephone() {
-        return this._telephone;
-    }
+  get telephone() {
+    return this._telephone;
+  }
 }
