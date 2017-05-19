b98e824ea3a14b11ac3e9342edc46e87a7bbc1cd
Eade <sam.eade@capgemini.com>
2017-05-19 11:38:35 +0100

    included missing material ui code

--- src/main/App.js
+++ src/main/App.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, browserHistory} from 'react-router-dom';
-import styled from "styled-components";
+import { MuiThemeProvider, darkBaseTheme } from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import styled from 'styled-components';
 import Home from './components/Home';
 import Catalogue from './components/Catalogue';
 import Contact from './components/Contact';
 import Cart from './components/Cart';
 import NavLinks from './components/NavLinks';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+const muiTheme = getMuiTheme(darkBaseTheme);
 
 const NavBar = styled.div`
 //insert styling here
@@ -13,20 +18,26 @@ const NavBar = styled.div`
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        injectTapEventPlugin();
+    }
+
     render() {
         return (
-            <Router history={browserHistory}>
-                <div>
-                    <NavBar>
-                        <NavLinks/>
-                    </NavBar>
-                    <Route path="/" component={Home}/>
-                    <Route path="/catalogue" component={Catalogue}/>
-                    <Route path="/contact" component={Contact}/>
-                    <Route path="/cart" component={Cart}/>
-                </div>
-            </Router>
-
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <Router history={browserHistory}>
+                    <div>
+                        <NavBar>
+                            <NavLinks/>
+                        </NavBar>
+                        <Route path="/" component={Home}/>
+                        <Route path="/catalogue" component={Catalogue}/>
+                        <Route path="/contact" component={Contact}/>
+                        <Route path="/cart" component={Cart}/>
+                    </div>
+                </Router>
+            </MuiThemeProvider>
         );
     }
 }
