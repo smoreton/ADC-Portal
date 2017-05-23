dce231270b5133ad5ac03ce6ed3a2ca8d4fa8c2e
Moreton <scott.moreton@capgemini.com>
2017-05-23 15:58:16 +0100

    Testing to see if prettier has been implemented correctly

--- src/main/App.js
+++ src/main/App.js
@@ -1,13 +1,12 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserHistory } from "react-router";
+import HomeTest from "./components/HomeTest";
+import CatalogueTest from "./components/CatalogueTest";
+import ContactTest from "./components/ContactTest";
+import CartTest from "./components/CartTest";
 
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import {BrowserHistory} from 'react-router';
-import HomeTest from './components/HomeTest';
-import CatalogueTest from './components/CatalogueTest';
-import ContactTest from './components/ContactTest';
-import CartTest from './components/CartTest';
-
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
@@ -17,10 +16,11 @@ class App extends Component {
         <Router history={browserHistory}>
           <div>
             <NavBar />
+
             <Route path="/" exact component={HomeTest} />
             <Route path="/catalogue" component={CatalogueTest} />
-            <Route path="/contact" component={ContactTest} />
             <Route path="/cart" component={CartTest} />
+            <Route path="/contact" component={ContactTest} />
           </div>
         </Router>
       </div>
