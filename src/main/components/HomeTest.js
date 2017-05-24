023b881e2ac5050921fbe1deee560a3b3a3b79f2
Sadler <tom.sadler@capgemini.com>
2017-05-24 11:34:29 +0100

    adding another dependancy

--- src/main/components/HomeTest.js
+++ src/main/components/HomeTest.js
@@ -14,6 +14,19 @@ const Container = styled.div`
   
 `;
 
+let comingSoon = [
+    {dateTime:'24/05/2017',header:'Jira is coming to us soon :)',description:'As an agreement has been reached with Jira, we will soon be able to offer their serviecs to projects'},
+    {dateTime:'23/05/2017',header:'Confluence is coming to us soon :)',description:'As an agreement has been reached with Confluence, we will soon be able to offer their serviecs to projects'},
+    {dateTime:'22/05/2017',header:'We have started a new Project',description:'This project (ADC) will be a good training ground'},
+    {dateTime:'01/01/2016',header:'Happy New Years',description:'As seen above'}
+];
+
+let issues = [
+    {dateTime:'20/05/2017',header:'There are major problems',description:'We are currently having major issues with our systems and will not abe able to process any requests'},
+    {dateTime:'19/05/2017',header:'There are minor problems',description:'We are currently having minor issues with our systems and will not abe able to process any requests'},
+    {dateTime:'02/05/2017',header:'No problems today',description:'End of testing information'},
+    {dateTime:'01/01/2016',header:'Happy New Years',description:'As seen above'}
+];
 
 let CardListing = React.createClass({
      render: function() {
@@ -24,7 +37,7 @@ let CardListing = React.createClass({
 
         return <div>{listCard}</div>;
      }
- }); 
+ });
 
 
 class HomeTest extends Component {
@@ -32,10 +45,10 @@ class HomeTest extends Component {
     return(
         <Row around="xs">
             <Container>
-                <CardListing listArray={comingSoonArray} />
+                <CardListing listArray={comingSoon} />
             </Container>
             <Container>
-                <CardListing listArray={issuesArray} />
+                <CardListing listArray={issues} />
             </Container>
         </Row>
     );
