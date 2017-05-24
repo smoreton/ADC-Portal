2658bd2a26c7d64ba78bcf91f2c7f62fed9172ec
Sadler <tom.sadler@capgemini.com>
2017-05-24 14:20:25 +0100

    included organised by date

--- src/main/App.js
+++ src/main/App.js
@@ -13,18 +13,27 @@ import AppNavBar from './components/AppNavBar';
 
 
 let comingSoon = [
-    {dateTime:'24/05/2017',header:'Jira is coming to us soon :)',description:'As an agreement has been reached with Jira, we will soon be able to offer their serviecs to projects'},
-    {dateTime:'23/05/2017',header:'Confluence is coming to us soon :)',description:'As an agreement has been reached with Confluence, we will soon be able to offer their serviecs to projects'},
-    {dateTime:'22/05/2017',header:'We have started a new Project',description:'This project (ADC) will be a good training ground'},
-    {dateTime:'01/01/2016',header:'Happy New Years',description:'As seen above'}
+    {dateTime:'23 March 2017',header:'Jira is coming to us soon :)',description:'As an agreement has been reached with Jira, we will soon be able to offer their serviecs to projects'},
+    {dateTime:'12 January 1998',header:'Confluence is coming to us soon :)',description:'As an agreement has been reached with Confluence, we will soon be able to offer their serviecs to projects'},
+    {dateTime:'11 January 2016',header:'We have started a new Project',description:'This project (ADC) will be a good training ground'},
+    {dateTime:'1 January 2016',header:'Happy New Years',description:'As seen above'}
 ];
 
 let issues = [
-    {dateTime:'20/05/2017',header:'There are major problems',description:'We are currently having major issues with our systems and will not abe able to process any requests'},
-    {dateTime:'19/05/2017',header:'There are minor problems',description:'We are currently having minor issues with our systems and will not abe able to process any requests'},
-    {dateTime:'02/05/2017',header:'No problems today',description:'End of testing information'},
-    {dateTime:'01/01/2016',header:'Happy New Years',description:'As seen above'}
+    {dateTime:'23 March 2017',header:'There are major problems',description:'We are currently having major issues with our systems and will not abe able to process any requests'},
+    {dateTime:'19 September 2017',header:'There are minor problems',description:'We are currently having minor issues with our systems and will not abe able to process any requests'},
+    {dateTime:'21 July 2016',header:'No problems today',description:'End of testing information'},
+    {dateTime:'1 January 2017',header:'Happy New Years',description:'As seen above'}
 ];
+comingSoon.sort(function(a, b) {
+    let dateA = new Date(a.dateTime), dateB = new Date(b.dateTime);
+    return dateB -  dateA;
+});
+
+issues.sort(function(a, b) {
+    let dateA = new Date(a.dateTime), dateB = new Date(b.dateTime);
+    return dateB - dateA;
+});
 
 //End of testing information
 
