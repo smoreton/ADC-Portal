d76a97189f5273d7e26cb66a18d40a499a4e077e
Eade <sam.eade@capgemini.com>
2017-05-25 18:22:08 +0100

    removed unused commented out code

--- src/main/components/ContactPage.js
+++ src/main/components/ContactPage.js
@@ -1,28 +1,8 @@
 import React, { Component } from "react";
 import ContactCard from "./ContactCard";
-//import styled from "styled-components";
 
 import "../App.css";
-/**
-const GridLayout = styled.div`
-width: 100%;
-display: flex;
-flex-direction: row;
-flex-wrap: wrap;
-`;
 
-const GridBox = styled.div`
-width: 33.333333333333%;
-height: 250px;
-padding: 0.25em;
-box-sizing: border-box;
-`;
-
-const GridBoxWrapper = styled.div`
-width: 100%;
-height: 100%;
-`;
-*/
 class ContactPage extends Component {
   render() {
     return <ContactCard contactList={this.props.contactList} />;
