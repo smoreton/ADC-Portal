f0459eb79f13c59700ef3b033113683cf98be43d
mark <markphughes17@hotmail.co.uk>
2017-05-30 16:05:05 +0100

    Created models and json for Issues and Coming Soon

--- src/main/App.js
+++ src/main/App.js
@@ -13,6 +13,12 @@ import Catalogue from "./components/CataloguePage";
 import ContactPage from "./components/ContactPage";
 import Checkout from "./components/CartPage";
 
+/**
+ * Model Imports
+ */
+import ComingSoon from "./model/comingSoon";
+import Issues from "./model/issues";
+
 const descriptionText =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in elit a turpis rhoncus commodo ac eu lorem. Nam auctor urna libero, mollis luctus diam euismod vitae. Nam auctor aliquam massa, tincidunt aliquet massa pretium eget. Aenean vitae tellus tincidunt, lacinia lectus vitae, volutpat nibh. Maecenas iaculis leo elit, semper pulvinar nisl dignissim lacinia. Proin dignissim dapibus augue, id ultricies odio. Pellentesque blandit nisi ante, ac commodo lacus dictum quis. Duis hendrerit nec enim non iaculis.";
 
---
+++ src/main/model/comingSoon.js
@@ -0,0 +1,26 @@
+export default class ComingSoon {
+  _id;
+  _dateTime;
+  _header;
+  _description;
+
+  constructor(id, dateTime, header, description) {
+    this._id = id + "";
+    this._dateTime = dateTime + "";
+    this._header = header + "";
+    this._description = description + "";
+  }
+
+  get Id() {
+    return this._id;
+  }
+  get dateTime() {
+    return this._dateTime;
+  }
+  get header() {
+    return this._header;
+  }
+  get description() {
+    return this._description;
+  }
+}
