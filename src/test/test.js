c0d4f7d7d3db0ac8c380f335c4b8a62e0a0aa6e0
Joe Dowd <dowdja@aston.ac.uk>
2017-05-30 14:23:41 +0100

    created working tests

--- src/test/test.js
+++ src/test/test.js
@@ -5,14 +5,35 @@ import { shallow } from "enzyme";
 import AppNavBar from "../main/components/AppNavBar";
 const assert = require("assert");
 
+function getLogoImage() {
+  const nav = shallow(<AppNavBar />);
+
+  // Looks for the child component with the id iconElementLeft
+  const iconElementLeft = nav.node.props.iconElementLeft;
+
+  // Filters through the child components of iconElementLeft to find the first image.
+  const logo = iconElementLeft.props.children.filter(el => {
+    return el.type === "img";
+  })[0];
+
+  return logo;
+}
+
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
 
 test("NavBar has image", () => {
-  const nav = shallow(<AppNavBar />);
-  console.log(nav.debug());
-  const logo = nav.find("img");
-  assert.equal(logo.prop("alt"), true);
+  const logo = getLogoImage();
+
+  // Asserts that the image component has alt text
+  assert.equal(logo.props.alt !== undefined, true);
+});
+
+test("NavBar has correct alt text", () => {
+  const logo = getLogoImage();
+
+  // Asserts that the image component has correct alt text
+  assert.equal(logo.props.alt === "ADC Service Portal", true);
 });
