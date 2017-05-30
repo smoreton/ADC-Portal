a63aed6d8930c4c9868aff63110a1cafb22c2071
Eade <sam.eade@capgemini.com>
2017-05-30 10:21:16 +0100

    included contact form on contact page

--- src/main/components/ContactForm.js
+++ src/main/components/ContactForm.js
@@ -1,16 +1,65 @@
 import React, { Component } from "react";
+import { Card } from "material-ui/Card";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
+import styled from "styled-components";
+
+const QueryCard = styled(Card)`
+width: 50%;
+margin: auto;
+`;
+
+const QuerySection = styled.div`
+display: flex;
+flex-direction: row;
+`;
+
+const QueryDetailBlock = styled.div`
+display: flex;
+flex-direction: column;
+padding: 0.25em;
+`;
+
+const QueryTextArea = styled.div`
+padding: 0.25em;
+`;
 
 class ContactForm extends Component {
-  handleSubmit(event) {
-    //set state with form data
-  }
+  //constructor
+
+  handleSubmit(event) {}
+
+  handleChange(event) {}
 
   render() {
-    render(
-      <form onSubmit={this.handleSubmit}>
-        <div>form data</div>
-        <input type="submit" value="Submit" />
-      </form>
+    return (
+      <QueryCard>
+        <form onSubmit={this.handleSubmit}>
+          <QuerySection>
+            <QueryDetailBlock>
+              <TextField hintText="Name" />
+              <TextField hintText="E-mail" />
+            </QueryDetailBlock>
+
+            <QueryDetailBlock>
+              <TextField hintText="Business Unit" />
+              <TextField hintText="Username" />
+            </QueryDetailBlock>
+          </QuerySection>
+
+          <QueryTextArea>
+            <TextField
+              hintText="Enter your Query here and this will be sent to our HelpDesk Inbox..."
+              fullWidth={true}
+              multiLine={true}
+              rows={1}
+              rowsMax={10}
+            />
+          </QueryTextArea>
+
+          <RaisedButton align="right" label="SEND" primary={true} />
+        </form>
+      </QueryCard>
     );
   }
 }
