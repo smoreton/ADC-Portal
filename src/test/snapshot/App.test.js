788af073f9d2f64f88a4a2f4418c562d31146a57
mark <markphughes17@hotmail.co.uk>
2017-05-30 10:04:41 +0100

    commit

--- src/test/snapshot/App.test.js
+++ src/test/snapshot/App.test.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../../main/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../../main/App";
+import { shallow } from "enzyme";
+import AppNavBar from "../../main/components/AppNavBar";
+const assert = require("assert");
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+});
+
+test("NavBar has image", () => {
+  const nav = shallow(<AppNavBar />);
+  const logo = nav.find("img");
+  assert.equal(logo.prop("alt"), true);
 });
---
+++ src/test/test.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+var nav = require("../main/components/AppNavBar.js");
+
+describe("Navbar tests", function() {
+  it("returns 1+1=2", function() {
+    assert.equal(calc.add(1, 1), 2);
+  });
+});
