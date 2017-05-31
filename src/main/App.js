c5e42f4edcd1bb17729a51802cd1e0904b2e5931
mark <markphughes17@hotmail.co.uk>
2017-05-31 13:07:39 +0100

    App.js now uses json object instead of local array const for issues and coming soon cards

--- src/main/App.js
+++ src/main/App.js
@@ -19,62 +19,15 @@ import Checkout from "./components/CartPage";
 import ComingSoon from "./model/comingSoon";
 import Issues from "./model/issues";
 
+var csJson = require("./data/comingSoon.json");
+const issuesJson = require("./data/issues.json");
+
 const descriptionText =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in elit a turpis rhoncus commodo ac eu lorem. Nam auctor urna libero, mollis luctus diam euismod vitae. Nam auctor aliquam massa, tincidunt aliquet massa pretium eget. Aenean vitae tellus tincidunt, lacinia lectus vitae, volutpat nibh. Maecenas iaculis leo elit, semper pulvinar nisl dignissim lacinia. Proin dignissim dapibus augue, id ultricies odio. Pellentesque blandit nisi ante, ac commodo lacus dictum quis. Duis hendrerit nec enim non iaculis.";
 
-const comingSoonArray = [
-  {
-    id: 1,
-    dateTime: "23 March 2017",
-    header: "Jira is coming to us soon :)",
-    description: "As an agreement has been reached with Jira, we will soon be able to offer their serviecs to projects"
-  },
-  {
-    id: 2,
-    dateTime: "12 January 1998",
-    header: "Confluence is coming to us soon :)",
-    description: "As an agreement has been reached with Confluence, we will soon be able to offer their serviecs to projects"
-  },
-  {
-    id: 3,
-    dateTime: "11 January 2016",
-    header: "We have started a new Project",
-    description: "This project (ADC) will be a good training ground"
-  },
-  {
-    id: 4,
-    dateTime: "1 January 2016",
-    header: "Happy New Years",
-    description: "As seen above"
-  }
-];
+const comingSoonArray = Object.values(csJson.messages);
+const issuesArray = Object.values(issuesJson.messages);
 
-const issuesArray = [
-  {
-    id: 1,
-    dateTime: "23 March 2017",
-    header: "There are major problems",
-    description: "We are currently having major issues with our systems and will not abe able to process any requests"
-  },
-  {
-    id: 2,
-    dateTime: "19 September 2017",
-    header: "There are minor problems",
-    description: "We are currently having minor issues with our systems and will not abe able to process any requests"
-  },
-  {
-    id: 3,
-    dateTime: "21 July 2016",
-    header: "No problems today",
-    description: "End of testing information"
-  },
-  {
-    id: 4,
-    dateTime: "1 January 2017",
-    header: "Happy New Years",
-    description: "As seen above"
-  }
-];
 comingSoonArray.sort(function(a, b) {
   let dateA = new Date(a.dateTime), dateB = new Date(b.dateTime);
   return dateB - dateA;
@@ -118,6 +71,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     injectTapEventPlugin();
+  }
+  populateComingSoon() {
+    for (var i = 0; i < csJson.length; i++) {
+      comingSoonArray.push(csJson[i]);
+    }
   }
 
   render() {
