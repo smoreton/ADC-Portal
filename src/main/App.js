4c0caf057a014bc7b1ff4553f48bb23136c0bafb
mark <markphughes17@hotmail.co.uk>
2017-05-31 15:19:04 +0100

    changed contacts to use model

--- src/main/App.js
+++ src/main/App.js
@@ -19,8 +19,9 @@ import Checkout from "./components/CartPage";
 import ComingSoon from "./model/comingSoon";
 import Issues from "./model/issues";
 
-var csJson = require("./data/comingSoon.json");
+const csJson = require("./data/comingSoon.json");
 const issuesJson = require("./data/issues.json");
+const contactsJson = require("./data/contacts.json");
 
 const descriptionText =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in elit a turpis rhoncus commodo ac eu lorem. Nam auctor urna libero, mollis luctus diam euismod vitae. Nam auctor aliquam massa, tincidunt aliquet massa pretium eget. Aenean vitae tellus tincidunt, lacinia lectus vitae, volutpat nibh. Maecenas iaculis leo elit, semper pulvinar nisl dignissim lacinia. Proin dignissim dapibus augue, id ultricies odio. Pellentesque blandit nisi ante, ac commodo lacus dictum quis. Duis hendrerit nec enim non iaculis.";
@@ -38,32 +39,7 @@ issuesArray.sort(function(a, b) {
   return dateB - dateA;
 });
 
-const contactList = [
-  {
-    profilePicture: "",
-    name: "Scott Moreton",
-    location: "Aston",
-    description: "Software Engineer, working within the ADC",
-    email: "scott.moreton@capgemini.com",
-    phoneNumber: "044789623579"
-  },
-  {
-    profilePicture: "",
-    name: "Sam Eade",
-    location: "Aston",
-    description: "Software Engineer working for the AIE",
-    email: "sam@sam.com",
-    phoneNumber: "0445987654321"
-  },
-  {
-    profilePicture: "",
-    name: "Joe Bloggs",
-    location: "Holborn",
-    description: "Software Engineer working for the AIE",
-    email: "random@random.com",
-    phoneNumber: "044598186321"
-  }
-];
+const contactList = Object.values(contactsJson.contacts);
 
 //End of testing information
 
@@ -71,11 +47,6 @@ class App extends Component {
   constructor(props) {
     super(props);
     injectTapEventPlugin();
-  }
-  populateComingSoon() {
-    for (var i = 0; i < csJson.length; i++) {
-      comingSoonArray.push(csJson[i]);
-    }
   }
 
   render() {
