991c44ccf4220e3f5e2e5151621921f61cdd23b2
Sadler <tom.sadler@capgemini.com>
2017-06-01 15:19:40 +0100

    refactored tile by creating more info button + changing image to be round

--- src/main/components/TileComponent.js
+++ src/main/components/TileComponent.js
@@ -5,38 +5,54 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Card } from "material-ui";
 import { Link } from "react-router-dom";
+import RaisedButton from "material-ui/RaisedButton";
 
-const Tile = styled(Card)`
-    padding-top: 2em;
-    height: 20em;
-    width: 20em;
-    margin: 3em;
-    background: #fff;
-    background-image: url(${props => props.src});
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center center;
-    
+const Tile = styled.div`
+    padding-top: 2em;
+    height: 15em;
+    width: 15em;
+    margin: 3em;
+    background: #fff;
+    background-size: contain;
+    borderRadius: 50%;
+    border: 5px solid #00BCD4;
+
 `;
 
+const Picture = styled.div`
+    background-size: contain;
+    width: 90%;
+    height: 90%;
+    background-image: url(${props => props.src});
+    background-repeat: no-repeat;
+    borderRadius: 35%;
+    background-position: center center;
+    margin: 0 auto;
+`;
 
 const Centralised = styled.div`
     text-align:center;
 `;
 
-class TileComponent extends Component {
-  render() {
-    return (
+class TileComponent extends Component {
+    render() {
+        return (
+            <Link to={"/service/" + this.props.service.id}>
+                <Centralised>
 
-      <Link to={"/checkout/" + this.props.service.id}>
-        <Centralised>
-          <Tile src={this.props.service.logoSource} />
-          <h1>{this.props.service.serviceTitle}</h1>
-        </Centralised>
+                    <Tile>
+                        <Picture  src={this.props.service.logoSource}/>
+                    </Tile>
 
-      </Link>
-    );
-  }
+                    <h1>{this.props.service.serviceTitle}</h1>
+
+                    <RaisedButton label="More Information" primary={true} />
+
+                </Centralised>
+
+            </Link>
+        );
+    }
 }
 
-export default TileComponent;
+export default TileComponent;
