10dbd39eae050e61926be78220e3f84af0dc0e1b
Sadler <tom.sadler@capgemini.com>
2017-06-05 16:54:35 +0100

    sorted out indentation

--- src/main/components/TileComponent.js
+++ src/main/components/TileComponent.js
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const Tile = styled.div`
-height: 175px;
-width: 175px;
-margin-top: 30px;
-background: #fff;
-background-size: contain;
-borderRadius: 100%;
-border: 1px solid #00BCD4;
-background-image: url(${props => props.src});
-background-repeat: no-repeat;
-background-position: center center;
-margin: 0 auto;
-margin-top: 30px;
+  height: 175px;
+  width: 175px;
+  margin-top: 30px;
+  background: #fff;
+  background-size: contain;
+  borderRadius: 100%;
+  border: 1px solid #00BCD4;
+  background-image: url(${props => props.src});
+  background-repeat: no-repeat;
+  background-position: center center;
+  margin: 0 auto;
+  margin-top: 30px;
 `;
 
 const ServiceName = styled.h1`
@@ -27,17 +27,17 @@ const Centralised = styled.div`
 `;
 
 const BulletContainer = styled.div`
-display: flex;
-margin-bottom: 20px;
-justify-content: center;
+  display: flex;
+  margin-bottom: 20px;
+  justify-content: center;
 `;
 
 const Bullet = styled.div`
-background-color: #00BCD4;
-padding: 5px 15px;
-color:black;
-text-decoration: underline ;
-border-radius: 25px;
+  background-color: #00BCD4;
+  padding: 5px 15px;
+  color:black;
+  text-decoration: underline ;
+  border-radius: 25px;
 `;
 
 class TileComponent extends Component {
