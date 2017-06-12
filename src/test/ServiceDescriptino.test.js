c92fe42475286bc5fe669887ebdff3cc2dbf0d41
Sadler <tom.sadler@capgemini.com>
2017-06-12 14:18:19 +0100

    added in the tests for each component

--- src/test/ServiceDescriptino.test.js
+++ src/test/ServiceDescriptino.test.js
@@ -3,16 +3,19 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 import PropTypes from "prop-types";
+import SelectField from "material-ui/SelectField";
+import RaisedButton from "material-ui/RaisedButton";
 
 import DescriptionCard from "../main/components/DescriptionCard";
 import ServiceDescription from "../main/components/ServiceDescription";
+import TileComponent from "../main/components/TileComponent";
 
 describe("ServiceDescription Component", () => {
   const muiTheme = getMuiTheme();
   const context = { muiTheme };
   const childContextTypes = { muiTheme: PropTypes.object };
 
-  const descriptionContent = "description text";
+  const descriptionContent = "description";
 
   const serviceValues = {
     1: {
@@ -48,9 +51,23 @@ describe("ServiceDescription Component", () => {
     }*/
   };
 
+  const businessUnitList = ["CBS", "AD&I", "HMRC"];
+
+  const userList = [
+    "15 users or less",
+    "16 to 25",
+    "26 to 50",
+    "51 to 100",
+    "101 to 500"
+  ];
+
   it("renders the correct components", () => {
     const wrapper = shallow(
-      <ServiceDescription service={1} serviceDetails={serviceValues} />,
+      <ServiceDescription
+        description={descriptionContent}
+        service={1}
+        serviceDetails={serviceValues}
+      />,
       {
         context: context,
         childContextTypes: childContextTypes
@@ -58,7 +75,7 @@ describe("ServiceDescription Component", () => {
     );
     expect(
       wrapper.contains(
-        <DescriptionCard description={serviceValues.description} />
+        <DescriptionCard description={serviceValues[1].description} />
       )
     ).to.equal(true);
   });
@@ -93,4 +110,103 @@ describe("ServiceDescription Component", () => {
 
     expect(wrapper.props().description).to.equal(descriptionContent);
   });
+
+  it("contains correct number of TileComponent components", () => {
+    const wrapper = mount(<ServiceDescription service={1} />, {
+      context,
+      childContextTypes
+    });
+    expect(wrapper.find(TileComponent)).to.have.length(1);
+  });
+
+  it("contains TileComponent component for tile information", () => {
+    const wrapper = mount(<ServiceDescription service={1} />, {
+      context,
+      childContextTypes
+    });
+
+    expect(wrapper.props().description).to.equal(descriptionContent);
+  });
+
+  /** it("contains correct number of Buttons", () => {
+    const wrapper = mount(
+      <ServiceDescription
+
+      />,
+      {
+        context,
+        childContextTypes
+      }
+    );
+    expect(wrapper.find(RaisedButton).to.have.length(1);
+  });
+
+  it("contains Button for add to cart button", () => {
+    const wrapper = mount(
+      <ServiceDescription
+
+      />,
+      {
+        context,
+        childContextTypes
+      }
+    );
+
+    expect(wrapper.props().description).to.equal(descriptionContent);
+  });
+
+  it("contains Button for submit button", () => {
+    const wrapper = mount(
+      <ServiceDescription
+
+      />,
+      {
+        context,
+        childContextTypes
+      }
+    );
+
+    expect(wrapper.props().description).to.equal(descriptionContent);
+  });*/
+
+  /**it("contains correct number of DropDowns", () => {
+    const wrapper = mount(
+      <ServiceDescription
+
+      />,
+      {
+        context,
+        childContextTypes
+      }
+    );
+    expect(wrapper.find(SelectField)).to.have.length(1);
+  });
+
+  it("contains DropDown for Users Required DropDown", () => {
+    const wrapper = mount(
+      <ServiceDescription
+
+      />,
+      {
+        context,
+        childContextTypes
+      }
+    );
+
+    expect(wrapper.props().select).to.equal(userList);
+  });
+
+  it("contains DropDown for BU DropDown", () => {
+    const wrapper = mount(
+      <ServiceDescription
+
+      />,
+      {
+        context,
+        childContextTypes
+      }
+    );
+
+    expect(wrapper.props().select).to.equal(businessUnitList);
+  });*/
 });
