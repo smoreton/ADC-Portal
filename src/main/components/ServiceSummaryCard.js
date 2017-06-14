7276b1ff33100b6a7223e7dfa835ff917de33027
Sadler <tom.sadler@capgemini.com>
2017-06-14 18:06:09 +0100

    added table into the service summary card

--- src/main/components/ServiceSummaryCard.js
+++ src/main/components/ServiceSummaryCard.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
 import { Card } from "material-ui/Card";
+import {
+  Table,
+  TableBody,
+  TableFooter,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from "material-ui/Table";
+import TextField from "material-ui/TextField";
+import Toggle from "material-ui/Toggle";
 
 import styled from "styled-components";
 
@@ -16,12 +27,105 @@ const Container = styled.div`
   overflow-y: auto;
 `;
 
+const styles = {
+  propContainer: {
+    width: 200,
+    overflow: "hidden",
+    margin: "20px auto 0"
+  },
+  propToggleHeader: {
+    margin: "20px auto 10px"
+  }
+};
+
+const tableData = [
+  {
+    Service: "Confluence",
+    UserRange: "15 users or less",
+    BU: "AD&I",
+    Cost: "£75"
+  },
+  {
+    Service: "Jira",
+    UserRange: "51 to 100",
+    BU: "HMRC",
+    Cost: "£300"
+  },
+  {
+    Service: "Atlassian",
+    UserRange: "16 to 25",
+    BU: "CBS",
+    Cost: "£179"
+  }
+];
+
 class ServiceSummaryCard extends Component {
+  state = {
+    fixedHeader: true,
+    fixedFooter: true,
+    stripedRows: false,
+    showRowHover: false,
+    selectable: true,
+    multiSelectable: false,
+    enableSelectAll: false,
+    deselectOnClickaway: true,
+    showCheckboxes: true,
+    height: "300px"
+  };
+
+  handleToggle = (event, toggled) => {
+    this.setState({
+      [event.target.name]: toggled
+    });
+  };
+
+  handleChange = event => {
+    this.setState({ height: event.target.value });
+  };
+
   render() {
     return (
       <SummaryCard>
 
-        <Container />
+        <Table
+          height={this.state.height}
+          fixedHeader={this.state.fixedHeader}
+          fixedFooter={this.state.fixedFooter}
+          selectable={this.state.selectable}
+          multiSelectable={this.state.multiSelectable}
+        >
+          <TableHeader
+            displaySelectAll={this.state.showCheckboxes}
+            adjustForCheckbox={this.state.showCheckboxes}
+            enableSelectAll={this.state.enableSelectAll}
+          >
+            <TableRow>
+              <TableHeaderColumn tooltip="Service">Service</TableHeaderColumn>
+              <TableHeaderColumn tooltip="UserRange">
+                User Range
+              </TableHeaderColumn>
+              <TableHeaderColumn tooltip="Business unit">
+                Business Unit
+              </TableHeaderColumn>
+              <TableHeaderColumn tooltip="Cost">Cost</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            displayRowCheckbox={this.state.showCheckboxes}
+            deselectOnClickaway={this.state.deselectOnClickaway}
+            showRowHover={this.state.showRowHover}
+            stripedRows={this.state.stripedRows}
+          >
+            {tableData.map((row, index) =>
+              <TableRow key={index}>
+                <TableRowColumn>{row.Service}</TableRowColumn>
+                <TableRowColumn>{row.UserRange}</TableRowColumn>
+                <TableRowColumn>{row.BU}</TableRowColumn>
+                <TableRowColumn>{row.Cost}</TableRowColumn>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
 
       </SummaryCard>
     );
