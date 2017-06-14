71b53cc87170492ded0c61270f8a7877f83ce281
Sadler <tom.sadler@capgemini.com>
2017-06-14 12:45:05 +0100

    created testing for the CartDataCapture

--- src/main/components/CartDataCapture.js
+++ src/main/components/CartDataCapture.js
@@ -4,43 +4,49 @@ import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
 import styled from "styled-components";
 
-const RaisedButtonStyle = () =>
-  <RaisedButton
-    name="RaisedButtonStyle"
-    label="User Emails"
-    primary={true}
-    style={{
-      boxShadow: "none"
-    }}
-  />;
-
 const DataCaptureCard = styled(Card)`
 width: 50%;
 margin: auto;
-padding:15px;
-margin-top:30px
+padding:20px;
+margin-top:2%
 `;
 
 const DataCaptureSection = styled.div`
 display: flex;
 flex-direction: row;
 flex-wrap: wrap;
-justify-content: space-between;
+justify-content: space-around;
 `;
 
 class CartDataCapture extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   handleSubmit = event => {};
+  addUser = event => {};
 
   render() {
     return (
       <DataCaptureCard>
-        <form onSubmit={this.handleSubmit}>
+        <form>
           <DataCaptureSection>
 
             <TextField hintText="Project Name" />
             <TextField hintText="Project Code" />
             <TextField hintText="Owner Email" />
-            <RaisedButtonStyle />
+            <RaisedButton
+              name="RaisedButtonStyle"
+              label="Add User Emails"
+              primary={true}
+              onTouchTap={this.addUser}
+              style={{
+                boxShadow: "none",
+                marginTop: "1%",
+                width: "100%",
+                maxWidth: "256px"
+              }}
+            />
 
           </DataCaptureSection>
 
