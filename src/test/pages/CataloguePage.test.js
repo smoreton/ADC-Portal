ab60168ea4c0f64fd793d718e940ee5a449f605a
Moreton <scott.moreton@capgemini.com>
2017-06-21 09:47:42 +0100

    Updated the tests to cater for changes made - they all pass

--- src/test/pages/CataloguePage.test.js
+++ src/test/pages/CataloguePage.test.js
@@ -15,8 +15,8 @@ describe("CataloguePage Component", () => {
   const context = { muiTheme };
   const childContextTypes = { muiTheme: PropTypes.object };
 
-  const serviceValues = {
-    1: {
+  const serviceValues = [
+    {
       serviceTitle: "Jira",
       logoSource:
         "https://www.atlassian.com/docroot/wac/resources/wac/img/social-icons/jira_logo.jpg ",
@@ -24,7 +24,7 @@ describe("CataloguePage Component", () => {
         "JIRA provides a variety of tools and functionality for agile teams for planning and delivery of their projects. It includes: Scrum boards Kanban boards Agile reporting Customizable workflows Agile roadmap planning ",
       category: "Tools/Software"
     },
-    2: {
+    {
       serviceTitle: "Confluence",
       logoSource:
         "https://www.atlassian.com/docroot/wac/resources/wac/img/social-icons/confluence_logo.jpg",
@@ -32,7 +32,7 @@ describe("CataloguePage Component", () => {
         "Create edit and collborate on meeting notes project plans product requirements and more. Include multimedia, dynamic content, and integrate with JIRA reporting.",
       category: "Tools/Software"
     },
-    3: {
+    {
       serviceTitle: "Atlassian",
       logoSource:
         "https://www.atlassian.com/docroot/wac/resources/wac/img/social-icons/atlassian_logo.jpg",
@@ -40,41 +40,32 @@ describe("CataloguePage Component", () => {
         "The ADC hosts the Atlassian suite in the Merlin datacentre. They maintain and support the Atlassian tools with a robust and reslilient network, and support staff based in Woking and Aston.",
       category: "Tools/Software"
     }
-  };
-
-  const servicesArray = ["1", "2", "3"];
+  ];
 
   it("renders the correct components", () => {
     const wrapper = mount(
       <MemoryRouter>
-        <CataloguePage
-          services={servicesArray}
-          serviceDetails={serviceValues}
-        />
+        <CataloguePage serviceDetails={serviceValues} />
       </MemoryRouter>,
       { context: context, childContextTypes: childContextTypes }
     );
-    //expect(wrapper.find(TileComponent)).to.equal(true);
-    expect(wrapper.find(TileComponent)).to.have.length(servicesArray.length);
+    expect(wrapper.find(TileComponent)).to.have.length(serviceValues.length);
   });
 
   it("the correct data is provided to render the service tile components", () => {
     const wrapper = mount(
       <MemoryRouter>
-        <CataloguePage
-          services={servicesArray}
-          serviceDetails={serviceValues}
-        />
+        <CataloguePage serviceDetails={serviceValues} />
       </MemoryRouter>,
       { context: context, childContextTypes: childContextTypes }
     );
 
     let tiles = wrapper.find(TileComponent);
-    expect(tiles).to.have.length(servicesArray.length);
+    expect(tiles).to.have.length(serviceValues.length);
 
     tiles.getNodes(tile => {
       expect(tile.props().service.serviceTitle).to.equal(
-        serviceValues[tile.props().service.id].serviceTitle
+        serviceValues[tile.props().service.serviceTitle]
       );
     });
   });
