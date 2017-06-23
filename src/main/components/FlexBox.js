e0cfa64b8822f32e94ef0cb4942dcc3cd6574cba
Eade <sam.eade@capgemini.com>
2017-06-23 16:03:09 +0100

    initial flex box component impl & css property override object

---
+++ src/main/components/FlexBox.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+export const GridLayout = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%
+    ${props => props.mixin.cssStyles}
+`;
+
+export const GridBox = styled.div`
+    width: 33%;
+    height: 33%;
+    ${props => props.mixin.cssStyles}
+`;
