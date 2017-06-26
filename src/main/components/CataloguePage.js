fd1f65070967dfeddff307e3be1a665578ce4fb8
Eade <sam.eade@capgemini.com>
2017-06-26 14:12:19 +0100

    abstracted category filter into its own component

--- src/main/components/CataloguePage.js
+++ src/main/components/CataloguePage.js
@@ -1,51 +1,14 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-
-import MenuItem from "material-ui/MenuItem";
-import SelectField from "material-ui/SelectField";
 
 import { GridLayout, GridBox } from "./FlexBox";
+import FilterCategoryComponent from "./FilterCategoryComponent";
 import CatalogueCardComponent from "./CatalogueCardComponent";
 
-const DropDownContainer = styled.div`
- width:100%;
-  margin:auto;
-  display: flex;
-  flex-direction: row;
-  justify-content:flex-end;
-  padding-right:10px;
-  `;
-
 class CataloguePage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: 0
-    };
-  }
-
-  handleChange = (event, value) => {
-    let newCategory;
-    switch (value) {
-      case 0:
-        newCategory = "all";
-        break;
-      case 1:
-        newCategory = "Tools/Software";
-        break;
-      case 2:
-        newCategory = "Infrastructure";
-        break;
-      case 3:
-        newCategory = "Networks";
-        break;
-      default:
-        newCategory = "all";
-    }
-
-    this.props.onServiceCategoryChange(newCategory);
-    this.setState({ value: value });
+  categoryChange = value => {
+    this.props.onServiceCategoryChange(value);
+    console.log("CataloguePage --> categoryChange --> value");
+    console.log(value);
   };
 
   renderServiceCatalogueCards = array => {
@@ -73,20 +36,10 @@ class CataloguePage extends Component {
   render() {
     return (
       <div>
-        <DropDownContainer>
-          <SelectField
-            floatingLabelStyle={{ color: "#00bcd4" }}
-            floatingLabelText="Category Type"
-            maxHeight={175}
-            value={this.state.value}
-            onChange={this.handleChange}
-          >
-            <MenuItem value={0} primaryText="All Categories" />
-            <MenuItem value={1} primaryText="Tools/Software" />
-            <MenuItem value={2} primaryText="Infrastructure" />
-            <MenuItem value={3} primaryText="Networks" />
-          </SelectField>
-        </DropDownContainer>
+        <FilterCategoryComponent
+          categoryList={this.props.serviceCategories}
+          onCategoryChange={this.categoryChange}
+        />
 
         <GridLayout>
           {this.createFilteredServiceArray(
---
+++ src/main/components/FilterCategoryComponent.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import MenuItem from "material-ui/MenuItem";
+import SelectField from "material-ui/SelectField";
+
+const DropDownContainer = styled.div`
+ width:100%;
+  margin:auto;
+  display: flex;
+  flex-direction: row;
+  justify-content:flex-end;
+  padding-right:10px;
+  ${props => props.mixin && props.mixin.cssStyles}
+  `;
+
+class FilterCategoryComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0
+    };
+  }
+
+  generateMenuItems = array => {
+    return array.map(arrayItem => {
+      return (
+        <MenuItem
+          key={arrayItem.id}
+          value={arrayItem.id}
+          primaryText={arrayItem.serviceTypeCategory}
+        />
+      );
+    });
+  };
+
+  handleChange = (event, value) => {
+    let newCategory = this.updateServiceCategory(
+      this.props.categoryList,
+      value
+    );
+
+    console.log("FilterCategoryComponent --> handleChange --> newCategory");
+    console.log(newCategory);
+
+    this.props.onCategoryChange(newCategory);
+    this.setState({ value: value });
+  };
+
+  updateServiceCategory = (array, value) => {
+    return array.filter(item => {
+      return item.serviceTypeCategory === array[value].serviceTypeCategory;
+    });
+  };
+
+  render() {
+    return (
+      <DropDownContainer>
+        <SelectField
+          floatingLabelStyle={{ color: "#00bcd4" }}
+          floatingLabelText="Category Type"
+          maxHeight={160}
+          value={this.state.value}
+          onChange={this.handleChange}
+        >
+          {this.generateMenuItems(this.props.categoryList)}
+        </SelectField>
+      </DropDownContainer>
+    );
+  }
+}
+
+export default FilterCategoryComponent;
