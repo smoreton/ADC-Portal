f800135d5927c39b51cd4992fbd234aa2d456584
Sadler <tom.sadler@capgemini.com>
2017-07-17 16:43:30 +0100

    created menu component

--- src/main/components/MenuComponent.js
+++ src/main/components/MenuComponent.js
@@ -1,3 +1,43 @@
-/**
- * Created by tsadler on 17/07/2017.
- */
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export const NavLinkReferences = [
+  {
+    link: "/catalogue",
+    name: "Service Catalogue"
+  },
+  {
+    link: "/checkout",
+    name: "Checkout"
+  },
+  {
+    link: "/contact",
+    name: "Contact Us"
+  },
+  {
+    link: "/faq",
+    name: "Need Help?"
+  }
+];
+
+class MenuComponent extends Component {
+  render() {
+    return (
+      <div>
+        {NavLinkReferences.map((item, index) =>
+          <ListStyle key={index}>
+            <li>
+              <Link to={item.link}>
+                <MenuItem>
+                  {item.name}
+                </MenuItem>
+              </Link>
+            </li>
+          </ListStyle>
+        )}
+      </div>
+    );
+  }
+}
+
+export default MenuComponent;
