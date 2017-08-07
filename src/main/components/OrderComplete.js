19364557708361d48c991b842f92dad60944aa0b
Olsen <morten.olsen@capgemini.com>
2017-08-07 15:07:53 +0100

    Set up Order Compelte and Failed pages. Need to remove shadow above the box image component

--- src/main/components/OrderComplete.js
+++ src/main/components/OrderComplete.js
@@ -3,10 +3,30 @@ import { Card } from "material-ui/Card";
 import styled from "styled-components";
 import AppNavBar from "./AppNavBar";
 import complete from "../../../public/img/Group.png";
+import boxes from "../../../public/img/bg-people-boxes.jpg";
 
 const OrderCard = styled(Card)`
-    width: 95%;
-    align: center;
+  width: 80%;
+  margin: auto;
+  margin-top: 20px;
+  padding-top: 50px;
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-bottom: 50px;
+  text-align: center;
+`;
+
+const BoxPicture = styled.div`
+  position: absolute;
+  width: 80%;
+  padding-top: 20%
+  margin-left: 10%;
+  margin-right:10%;
+  background-image: url(${boxes});
+  background-position: center; 
+  background-repeat: no-repeat;
+  background-color: #ffffff;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px;
 `;
 
 class OrderComplete extends Component {
@@ -21,6 +41,8 @@ class OrderComplete extends Component {
             The ADC team will contact you shortly to complete the order
           </div>
         </OrderCard>
+
+        <BoxPicture />
       </div>
     );
   }
