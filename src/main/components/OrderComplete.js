132004c9a4cf1ea48b0a86c3c78cc3c1d914e60c
Moreton <scott.moreton@capgemini.com>
2017-08-08 13:34:55 +0100

    Added some styling the to the orderComplete component

--- src/main/components/OrderComplete.js
+++ src/main/components/OrderComplete.js
@@ -1,26 +1,54 @@
 import React, { Component } from "react";
-import { Card } from "material-ui/Card";
+import Paper from "material-ui/Paper";
 import styled from "styled-components";
 import AppNavBar from "./AppNavBar";
 import complete from "../../../public/img/Group.png";
+import Boxes from "../../../public/img/bg-people-boxes.jpg";
 
-const OrderCard = styled(Card)`
-    width: 95%;
-    align: center;
+const TextPos = styled.div`
+  left: 45%;
 `;
+
+const BoxLogo = styled.div`
+  position: absolute;
+  top: 55%;
+  left: 25%;
+`;
+
+const Logo = styled.div`
+
+  top: 33%;
+  left: 50%;
+`;
+
+const style = {
+  width: "100%",
+  maxWidth: "none",
+  height: 700,
+  maxHeight: "none",
+  textAlign: "center",
+  display: "inline-block"
+};
 
 class OrderComplete extends Component {
   render() {
     return (
       <div>
         <AppNavBar />
-        <OrderCard>
-          <div>
+
+        <Paper style={style} zDepth={1}>
+          <h1>Order Submitted</h1>
+          <Logo>
             <img src={complete} alt="Success" />
-            <h1>Order Submitted</h1>
-            The ADC team will contact you shortly to complete the order
-          </div>
-        </OrderCard>
+          </Logo>
+
+          The ADC team will contact you shortly to complete the order
+
+          <BoxLogo>
+            <img src={Boxes} alt="Success" />
+
+          </BoxLogo>
+        </Paper>
       </div>
     );
   }
